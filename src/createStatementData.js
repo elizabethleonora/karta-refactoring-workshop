5784b02c24890a4e8fd68ac16dbe6f6d584544d9
e211320 <oleksander.dushyn@evry.com>
2019-07-17 13:41:56 +0200

    add comedy and tragedy calculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -30,7 +30,14 @@ function createStatementData(invoice, plays) {
     }
 
     function createPerformanceCalculator(perf, play) {
-        return new PerformanceCalculator(perf, play);
+        switch (play.type) {
+            case 'tragedy':
+                return new TragedyCalculator(perf, play);
+            case 'comedy':
+                return new ComedyCalculator(perf, play);
+            default:
+                throw new Error(`unknow type: ${play.type}`);
+        }
     }
 }
 
@@ -41,35 +48,35 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
+        throw new Error('subclass responsibility');
+    }
 
-        switch (this.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknow type: ${this.play.type}`);
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
         }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
         return result;
     }
 
     get volumeCredits() {
-        let result = 0;
-        // add volume credits
-        result += Math.max(this.performance.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
-
-        return result;
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
